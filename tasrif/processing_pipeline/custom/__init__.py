45a80b8942b4dd4428780c2169560771d029138d
Ummar Abbas <uabbas@hbku.edu.qa>
2021-02-04 08:32:46 +0000

    Merged PR 4244: Add CreateFeatureOperator
    Add CreateFeatureOperator
    
    Related work items: #6808

---
+++ tasrif/processing_pipeline/custom/__init__.py
@@ -0,0 +1,4 @@
+"""Module to export customized pipeline operators
+"""
+
+from tasrif.processing_pipeline.custom.create_feature_operator import CreateFeatureOperator
---
+++ tasrif/processing_pipeline/custom/create_feature_operator.py
@@ -0,0 +1,59 @@
+"""
+Operator to create a new column feature from existing column features
+"""
+from tasrif.processing_pipeline import ProcessingOperator
+
+
+class CreateFeatureOperator(ProcessingOperator):
+    """
+
+      Creates a new column based feature from existing features in the data frame.
+      This operator works on a 2D data frames where the columns represent the features.
+      The defintiion of the new feature is passed as a lambda argument.
+
+      Examples
+      --------
+
+      >>> import pandas as pd
+      >>>
+      >>> from tasrif.processing_pipeline.custom import CreateFeatureOperator
+      >>>
+      >>> df0 = pd.DataFrame([['tom', 10, 2], ['nick', 15, 2], ['juli', 14, 12]],
+      >>>                     columns=['name', 'work_hours', 'off_hours'])
+      >>>
+      >>> operator = CreateFeatureOperator(
+      >>>    feature_name="total_hours",
+      >>>    feature_creator=lambda df: df['work_hours'] + df['off_hours'])
+      >>> df0 = operator.process(df0)
+      >>>
+      >>> print(df0)
+    """
+
+    def __init__(self, feature_name, feature_creator):
+        """Creates a new instance of CreateFeatureOperator
+
+        Parameters
+        ----------
+        feature_name : str
+            Name of the new feature
+        feature_creator : lambda
+            lambda operator defining how the new feature can be created from existing column features.
+        """
+        self.feature_name = feature_name
+        self.feature_creator = feature_creator
+
+
+    def process(self, *data_frames):
+        """Processes the passed data frame as per the configuration define in the constructor.
+
+        Returns
+        -------
+        pd.DataFrame -or- list[pd.DataFrame]
+            Processed dataframe(s) resulting from applying the operator
+        """
+        processed = []
+        for data_frame in data_frames:
+            data_frame[self.feature_name] = data_frame.apply(self.feature_creator, axis=1)
+            processed.append(data_frame)
+
+        return processed
