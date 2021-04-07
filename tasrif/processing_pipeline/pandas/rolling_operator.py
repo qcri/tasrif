4634b32a83cb77c0698ade084ae6f912f99488d0
abalhomaid <ahomaidcmuq@gmail.com>
2021-04-07 04:24:09 +0300

    Add pandas RollingOperator

---
+++ tasrif/processing_pipeline/pandas/rolling_operator.py
@@ -0,0 +1,65 @@
+"""
+Rolling Operator
+"""
+from tasrif.processing_pipeline import ProcessingOperator
+
+class RollingOperator(ProcessingOperator):
+    """
+
+    Examples
+    --------
+  
+    >>> import pandas as pd
+    >>> import numpy as np
+    >>> from tasrif.processing_pipeline.pandas import RollingOperator
+    >>> 
+    >>> 
+    >>> df = pd.DataFrame({'B': [0, 1, 2, 3, 4]})
+    >>> 
+    >>> op = RollingOperator(2)
+    >>> op.process(df)[0].sum()
+
+        B
+    0   NaN
+    1   1.0
+    2   3.0
+    3   5.0
+    4   7.0
+
+    """
+
+    def __init__(self, window, select=None, **kwargs):
+        """Creates a new instance of RollingOperator
+
+        Parameters
+        ----------
+        window: int, offset, or BaseIndexer subclass
+        select: select the columns of a groupby object
+        kwargs: Arguments to pandas pd.groupby function
+
+        """
+        self.window = window
+        self.select = select
+        self.kwargs = kwargs
+        super().__init__()
+
+
+    def process(self, *data_frames):
+        """Processes the passed data frame as per the configuration define in the constructor.
+
+        Returns
+        -------
+        pd.DataFrame -or- list[pd.DataFrame]
+            Processed dataframe(s) resulting from applying the operator
+        """
+
+
+        processed = []
+        for data_frame in data_frames:
+            if self.select:
+                data_frame = data_frame.rolling(self.window, **self.kwargs)[self.select]
+            else:
+                data_frame = data_frame.rolling(self.window, **self.kwargs)
+            processed.append(data_frame)
+
+        return processed
