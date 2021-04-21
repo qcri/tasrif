c8391abc4c75a3d2ce0db28926bdf2565c8d483d
abalhomaid <ahomaidcmuq@gmail.com>
2021-04-21 08:33:43 +0300

    Add InvCode Add ValidatorOperator Add FlagEpochActivityLessThanOperator Add FlagDayIfValidEpochsSmallerThanOperator Add FlagDayIfValidEpochsLargerThanOperator Add FlagEpochNullColsOperator Add FlagDayIfNotEnoughConsecutiveDaysOperator Add ValidationReportOperator Add RemoveFlaggedDaysOperator

--- tasrif/processing_pipeline/__init__.py
+++ tasrif/processing_pipeline/__init__.py
@@ -6,3 +6,5 @@ from tasrif.processing_pipeline.processing_pipeline import ProcessingPipeline
 from tasrif.processing_pipeline.sequence_operator import SequenceOperator
 from tasrif.processing_pipeline.compose_operator import ComposeOperator
 from tasrif.processing_pipeline.noop_operator import NoopOperator
+from tasrif.processing_pipeline.validator_operator import InvCode
+from tasrif.processing_pipeline.validator_operator import ValidatorOperator
---
+++ tasrif/processing_pipeline/custom/flag_epoch_null_cols_operator.py
@@ -0,0 +1,48 @@
+"""
+Operator to flag columns that contain null
+"""
+from tasrif.processing_pipeline import ValidatorOperator
+from tasrif.processing_pipeline import InvCode
+
+class FlagEpochNullColsOperator(ValidatorOperator):
+    """
+    For each on of the columns in ``col_list``, this method marks as invalid (InvCode.FLAG_EPOCH_NULL_VALUE) if the value for an epoch is None/Null.
+
+    :param col_list: List of columns to check for Null/None values.
+    """
+
+    def __init__(self, 
+                 col_list: list,
+                 **kwargs):
+        """Creates a new instance of CreateFeatureOperator
+
+        Parameters
+        ----------
+        features : list
+            list of features to select
+        conditions : lambda function that result in pandas row indexing dataframe
+        (a dataframe of trues and falses), see example.
+        """
+        super().__init__(**kwargs)
+        self.col_list = col_list
+
+
+    def process(self, *data_frames):
+        """
+        For each on of the columns in ``col_list``, this method marks as invalid (InvCode.FLAG_EPOCH_NULL_VALUE) if the value for an epoch is None/Null.
+
+        :param col_list: List of columns to check for Null/None values.
+        """
+
+        super().process(*data_frames)
+
+        processed = []
+        for data_frame in data_frames:
+            for col in self.col_list:
+                if col not in data_frame.keys():
+                    raise KeyError("Col %s is not available for the dataframe" % (col))
+
+                data_frame.loc[data_frame[col].isnull(), self.invalid_col] |= InvCode.FLAG_EPOCH_NULL_VALUE
+                processed.append(data_frame)
+
+        return processed
