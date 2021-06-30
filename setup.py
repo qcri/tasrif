e520ebff5c7c63dde415c15d2dc67f35bc10eef7
Fadhil Abubaker <fabubaker@hbku.edu.qa>
2021-06-30 06:50:35 +0000

    Merged PR 4918: Run notebook-tests on PR builds
    The tasrif-ci pipeline has been modified to run the notebook tests on CI builds.
    
    Pull requests and merges will now trigger notebook tests in addition to the other CI jobs.
    
    Related work items: #7867

--- setup.py
+++ setup.py
@@ -15,8 +15,7 @@ setup(
         'pandas >= 1.1.1',
         'numpy >= 1.19.5',
         'pyjq >= 2.5.1',
-        'matplotlib >= 3.0.0',
-        'ummalqura>=2.0.1'
+        'ummalqura >= 2.0.1'
     ],
     # numpy also needs to be specified in setup_requires,
     # see https://github.com/numpy/numpy/issues/2434#issuecomment-65252402
