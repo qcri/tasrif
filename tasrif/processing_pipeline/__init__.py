1edc22ee217a0369d8abbe0621743a1e7668bfef
Fadhil Abubaker <fabubaker@hbku.edu.qa>
2021-08-15 10:37:23 +0000

    Merged PR 5052: Remove IterateOperator
    This operator is redundant since it does what a generic ProcessingOperator should do.
    
    Related work items: #8036

--- tasrif/processing_pipeline/__init__.py
+++ tasrif/processing_pipeline/__init__.py
@@ -6,6 +6,5 @@ from tasrif.processing_pipeline.processing_pipeline import ProcessingPipeline
 from tasrif.processing_pipeline.sequence_operator import SequenceOperator
 from tasrif.processing_pipeline.compose_operator import ComposeOperator
 from tasrif.processing_pipeline.noop_operator import NoopOperator
-from tasrif.processing_pipeline.iterate_operator import IterateOperator
 from tasrif.processing_pipeline.generator_processing_operator import GeneratorProcessingOperator
 from tasrif.processing_pipeline.split_join_operator import SplitJoinOperator
