aa4cbb08bb23f86bf9b0b050a4848dc8c721ee77
Fadhil Abubaker <fabubaker@hbku.edu.qa>
2021-08-22 08:36:15 +0000

    Merged PR 5079: Remove GeneratorProcessingOperator
    The functionality of this operator can be replicated by the Map/Reduce Processing Operators.
    
    Related work items: #8035

--- tasrif/processing_pipeline/__init__.py
+++ tasrif/processing_pipeline/__init__.py
@@ -6,7 +6,6 @@ from tasrif.processing_pipeline.processing_pipeline import ProcessingPipeline
 from tasrif.processing_pipeline.sequence_operator import SequenceOperator
 from tasrif.processing_pipeline.compose_operator import ComposeOperator
 from tasrif.processing_pipeline.noop_operator import NoopOperator
-from tasrif.processing_pipeline.generator_processing_operator import GeneratorProcessingOperator
 from tasrif.processing_pipeline.split_join_operator import SplitJoinOperator
 from tasrif.processing_pipeline.map_processing_operator import MapProcessingOperator
 from tasrif.processing_pipeline.reduce_processing_operator import ReduceProcessingOperator
