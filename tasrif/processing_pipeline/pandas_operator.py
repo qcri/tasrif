09c3d214a35459d4254427c863108c442f84df6f
Abdulaziz Al-Homaid <ahomaidcmuq@gmail.com>
2022-03-10 14:20:49 +0300

    Add warning to Pandas Operator if one input is empty (#41)
    * Add warning to Pandas Operator
    
    * Add specific validator for JsonNormalizeOperator
    
    Co-authored-by: Aziz <abalhomaid@QCRI.org>

--- tasrif/processing_pipeline/pandas_operator.py
+++ tasrif/processing_pipeline/pandas_operator.py
@@ -1,5 +1,7 @@
 """Module that defines the PandasOperator class
 """
+
+import warnings
 from tasrif.processing_pipeline.processing_operator import ProcessingOperator
 
 
@@ -11,3 +13,20 @@ class PandasOperator(ProcessingOperator):
         super().__init__(observers=kwargs.get("observers", []))
         if "observers" in kwargs:
             del kwargs["observers"]
+
+
+    def _validate(self, *data_frames):
+        """
+        Validation hook that is run before any processing happens. Checks if
+        any data_frame is empty to raise a warning
+
+        Args:
+            *data_frames (list of pd.DataFrame):
+              Variable number of pandas dataframes to be processed
+
+        Raises:
+            Warning: If any input is empty. # noqa: DAR402
+        """
+        for data_frame in data_frames:
+            if data_frame.empty:
+                warnings.warn('One or more inputs are empty.')
