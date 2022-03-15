20d8bc3c404d194572f4be6e72cb567cd9a77b07
Ummar Abbas <uabbas@hbku.edu.qa>
2022-03-15 11:09:14 +0300

    Update version number to v0.1.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/qcri/tasrif",
-    version="0.0.7",
+    version="0.1.0",
     packages=find_packages(),
     license=" BSD-3-Clause",
     classifiers=[
